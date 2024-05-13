02991762cf2b722dc2ecdd2c98084155adc17cd1
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-05-13 06:00:51 -0400

    display pagination buttons

--- app.js
+++ app.js
@@ -4,12 +4,21 @@ import paginate from "./paginate.js";
 import displayButtons from "./displayButtons.js";
 
 const title = document.querySelector(".section-title h1");
+const btnContainer = document.querySelector(".btn-container");
+
+let index = 0;
+let pages = [];
+
+const setupUI = () => {
+  displayFollowers(pages[index]);
+  displayButtons(btnContainer, pages, index);
+};
 
 const init = async () => {
   const followers = await fetchFollowers();
-  displayFollowers(followers);
   title.textContent = "Pagination";
-  const pages = paginate(followers);
+  pages = paginate(followers);
+  setupUI();
 };
 
 window.addEventListener("load", init);
