0651f8cdb4bd0506c48c98d3e2fcdb75210b11f4
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-05-12 21:27:15 -0400

    display followers

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-import fetchFollowers from './fetchFollowers.js'
-import displayFollowers from './displayFollowers.js'
-import paginate from './paginate.js'
-import displayButtons from './displayButtons.js'
+import fetchFollowers from "./fetchFollowers.js";
+import displayFollowers from "./displayFollowers.js";
+import paginate from "./paginate.js";
+import displayButtons from "./displayButtons.js";
+
+const title = document.querySelector(".section-title h1");
+
+const init = async () => {
+  const followers = await fetchFollowers();
+  displayFollowers(followers);
+  title.textContent = "Pagination";
+};
+
+window.addEventListener("load", init);
